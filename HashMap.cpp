deba9bd309019bbcbf6b662ed1b19df70e2734bf
adialexiu <adi.alexiu@outlook.com>
2019-05-25 17:44:03 +0300

    fixed errors for the push function

--- HashMap.cpp
+++ HashMap.cpp
@@ -2,11 +2,7 @@
 
 template <class K, class V, class F>
 HashMap<K, V, F>::HashMap() {
-    /*
-    this->_Prim = this->_Ultim = NULL;
-    this->_Size = 0;
-    */
-    _Size = 100;
+    _Size = 0;
     _M = new HashNode<K, V> *[_Size];
 }
 
@@ -17,7 +13,7 @@ void HashMap<K, V, F>::push(const K& k, const V& v) {
     HashNode<K, V> *last = NULL;
     int i = 0;
 
-    cout << funcVal << endl;
+    //cout << funcVal << endl;
 
     while(aux != NULL && aux->getKey() != k) {    //daca nu s-a terminat lista si daca nu s-a mai gasit cheia curenta
         last = aux;
@@ -25,6 +21,13 @@ void HashMap<K, V, F>::push(const K& k, const V& v) {
         i++;
     }
 
+/*
+    do {
+        i++;
+        last = aux;
+        aux = aux->getNext();
+    }while(aux != NULL && aux->getKey() != k);
+*/
     if(aux == NULL) {   //daca s-a ajuns la finalul listei la cautare
         aux = new HashNode<K, V>(k, v);
         if(i == 0) {    //inserez la inceput daca nu mai e niciun element in lista
@@ -56,23 +59,25 @@ void HashMap<K, V, F>::remove(const K& k) {
     unsigned long funcVal = _F(k);
     HashNode<K, V> *aux = _M[funcVal];
     HashNode<K, V> *last = NULL;    //retin ultimul element din lista inainte de aux
-    int i = 0;
+    bool found = false;
+    int i = 0, pozK = 0;
 
-    cout << funcVal << endl;
-
-    while(aux != NULL && aux->getKey() != k) {    //daca nu s-a terminat lista si daca nu s-a mai gasit cheia curenta
-        last = aux;
-        aux = aux->getNext();
+    do {
         i++;
-    }
+        last = aux;
+        if(aux->getKey() == k) {
+            found = true;
+            pozK = i;
+        }
+        aux = aux->getNext();
+    }while(aux != NULL && aux->getKey() != k);      //daca nu s-a terminat lista si daca nu s-a mai gasit cheia curenta
 
-    if(i == 0) {    //daca nu s-a gasit elementul cu cheia respectiva ies din functie
+    if(found == false) {    //daca nu s-a gasit elementul cu cheia respectiva ies din functie
         return;
     }
 
-    if(i == 1) {    //verific daca e primul element din lista
-        _M[funcVal] = aux->getNext();
-        delete aux;
+    if(pozK == 1) {    //verific daca e primul element din lista
+        _M[funcVal] = aux;
         return;
     }
 
@@ -84,15 +89,13 @@ template <typename K, typename V, typename F>
 V HashMap<K, V, F>::get(const K& k) {
     unsigned long funcVal = _F(k);
     HashNode<K, V> *aux = _M[funcVal];
-/*
+
     if(aux == NULL) {   //daca nu exista cheia returnez NULL
-        cout << "KOD\n";
         return NULL;
     }
-*/
-    cout << funcVal << endl;
+
+    //cout << funcVal << endl;
     while(aux != NULL) {    //altfel caut in lista pana il gasesc
-            cout << aux->getKey() << endl;
         if(aux->getKey() == k) {
             return aux->getValue();
         }
--- main.cpp
+++ main.cpp
@@ -2,17 +2,17 @@
 #include "HashMap.cpp"
 //#include "KeyHash.h"
 using namespace std;
-/*
-struct HashKey {
-    unsigned long operator() (const )
-};
-*/
+
 int main()
 {
     HashMap<int, int, KeyHash<int> > H;
-    H.push(12, 1234);
-    cout << H.get(12) << endl;
-    H.remove(12);
+    int p = 12, x = 13;
+    H.push(p, 1234);
+    H.push(x, 5678);
+    cout << H.get(p) << ' ' << H.get(x) << endl;
+    //H.remove(p);
+    H.remove(p);
+    cout << H.get(x) << endl;
     //H.push(134, 1234);
     //cout << H.get(134);
     //H.push(2, 5678);
